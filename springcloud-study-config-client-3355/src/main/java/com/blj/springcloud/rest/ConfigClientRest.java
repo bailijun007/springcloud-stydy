1d90e7ed35de7a229a86c0659b53d1c3cdc1797d
白礼军 <18756096010@163.com>
2019-10-13 19:24:37 +0800

    SpringCloud Config 客户端配置与测试

--- springcloud-study-config-client-3355/src/main/java/com/blj/springcloud/rest/ConfigClientRest.java
+++ springcloud-study-config-client-3355/src/main/java/com/blj/springcloud/rest/ConfigClientRest.java
@@ -1,7 +1,7 @@
 package com.blj.springcloud.rest;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
